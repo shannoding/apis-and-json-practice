2476a9de3b77e5e3c00e7130741f0d18f2c2c5f4
Shannon Ding <shannon_ding@yahoo.com>
2017-07-06 09:01:24 -0700

    movie poster

---
+++ JSON-API-Swift-Starter-swift3/API-Sandbox/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  API-Sandbox
+//
+//  Created by Dion Larson on 6/24/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+import AlamofireImage
+import AlamofireNetworkActivityIndicator
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var rightsOwnerLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        exerciseOne()
+//        exerciseTwo()
+//        exerciseThree()
+        
+        let apiToContact = "https://itunes.apple.com/us/rss/topmovies/limit=25/json"
+        // This code will call the iTunes top 25 movies endpoint listed above
+        Alamofire.request(apiToContact).validate().responseJSON() { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    
+                    let randomMovieJSON = json["feed"]["entry"][Int(arc4random_uniform(25))]
+                    let randomMovie = Movie(json: randomMovieJSON)
+                    
+                    self.movieTitleLabel.text = randomMovie.name
+                    self.rightsOwnerLabel.text = randomMovie.rightsOwner
+                    self.releaseDateLabel.text = randomMovie.releaseDate
+                    self.priceLabel.text = String(randomMovie.price)
+                    let randomPosterLink = randomMovie.link
+                    self.loadPoster(urlString: String(randomPosterLink))
+                    
+                    
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Updates the image view when passed a url string
+    func loadPoster(urlString: String) {
+        print("WHY IN TARNATION")
+        posterImageView.af_setImage(withURL: URL(string: urlString)!)
+    }
+    
+    @IBAction func viewOniTunesPressed(_ sender: AnyObject) {
+        
+    }
+    
+}
+
